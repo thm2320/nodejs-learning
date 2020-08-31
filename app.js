de29cd82250975ec3904eb51564493edd094368d
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-08-31 16:11:02 +0800

    Update app.js
    Ch62 expressjs behind the scene

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
@@ -11,9 +10,8 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('In another middleware');
+  res.send('<h1>Hello</h1>')
 });
 
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
