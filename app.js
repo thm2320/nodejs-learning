7c5a4affcb402f26be046b8b211007daa31c0c93
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-08-31 16:38:59 +0800

    Add routes to the app

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express = require('express');
 
 const app = express();
 
-//first middleware
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Allows the request to continuse to the next middleware in line
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.use('/', (req, res, next) => {
+  console.log('always runs!');
+  console.log(req.url)
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('In Add Product');
+  res.send('<h1>Add product</h1>')
+});
+
+app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello</h1>')
 });
+
+
 
 
 app.listen(3000);
