41cd7bec0ab49109c1a2601ebeb5b8620089507e
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-01 11:18:37 +0800

    Ch68 Filtering Path
    path prefix is defined by the app.use

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(bodyParser.urlencoded({extended:false}));
 
 //the order here will affecting the route behavior if the routes are using "router.use" method
-app.use(adminRoutes);
+app.use('/admin',adminRoutes); // filter the route, all url start with '/admin' with go this router
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  //method cascading
+  res.status(404).send('<h1>Page not found</h1>')
+});
 
 app.listen(3000);
