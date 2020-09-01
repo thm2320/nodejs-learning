8057afb88da2b611fbc4c0c8fc584c4eec6cf7ce
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-01 09:45:14 +0800

    Ch64-65
    BodyParsing and use the "POST" method

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use('/', (req, res, next) => {
-  console.log('always runs!');
-  console.log(req.url)
-  next();
-});
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In Add Product');
-  res.send('<h1>Add product</h1>')
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
+app.post('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+})
+
 app.use('/', (req, res, next) => {
-  console.log('In another middleware');
   res.send('<h1>Hello</h1>')
 });
 
