ffa9a9c056135641e7dcf569684b9c5812933f71
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-03 17:27:51 +0800

    S9 Edit Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,21 +1,49 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, description, price } = req.body
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  });
+
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const {
+    productId, title, imageUrl, description, price
+  } = req.body;
+  console.log('postEditProduct')
+  console.log(productId, title, imageUrl, description, price)
+  const updatedProduct = new Product(productId, title, imageUrl, description, price)
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
