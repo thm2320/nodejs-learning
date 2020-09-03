d6fb154cd416b27ed310050d27d507a3327e2a0d
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-03 10:02:54 +0800

    S8 initialize all the routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,14 +19,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render('shop/product-list', {
+    res.render('admin/products', {
       products: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCss: true
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
   });
-
 };
