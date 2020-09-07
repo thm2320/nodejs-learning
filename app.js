e58573478b8ebf6f77aa88ae117e2e5487354c09
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-07 10:34:56 +0800

    S11 Cart model and fetch cart

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 //ejs template engine
 app.set('view engine', 'ejs');
@@ -38,6 +40,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   //.sync({ force: true })
@@ -53,9 +59,11 @@ sequelize
     return user;
 
   }).then(user => {
-    // console.log(user);
+    return user.createCart();
+  }).then(cart => {
     app.listen(3000);
-  }).catch(err => {
+  })
+  .catch(err => {
     console.log(err)
   });
 
