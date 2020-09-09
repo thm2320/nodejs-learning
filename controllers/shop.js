444b540696808c91df52a29d10bb9a5bb54e6215
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-09 10:55:13 +0800

    S13 define "addToCart" function for user schema

--- controllers/shop.js
+++ controllers/shop.js
@@ -59,7 +59,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then(product => {
-      return req.user.addToCart(product)
+      return req.user.addToCart(product);
     })
     .then(result => {
       console.log(result);
