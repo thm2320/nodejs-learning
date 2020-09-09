8235e429f7cf86bd9492372d6dcd0061c46d89f7
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-09 12:01:01 +0800

    S13 Create Order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -82,9 +83,33 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user.addOrder()
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate() // for populate from a document, it is to return a promise, not required for .find
+    .then(user => {
+      console.log(user.cart.items)
+      const products = user.cart.items.map(i => {
+        return {
+          quantity: i.quantity,
+          product: { ...i.productId._doc } // .doc get all the details in the product
+        }
+      })
+
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+
+      return order.save()
+    })
     .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      
       res.redirect('/orders');
     })
     .catch(err => console.log(err))
