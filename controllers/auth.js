eb029e06f82911f30d01be505ef787ba481ab483
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-17 11:34:38 +0800

    S15 login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,14 +19,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5f5839ded767a01ea867574d")
+  const { email, password } = req.body
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect('/');
-      })
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              return res.redirect('/');
+            })
+          }
+          res.redirect('/login')
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login')
+        })
+
+
     })
     .catch(err => console.log(err));
 };
