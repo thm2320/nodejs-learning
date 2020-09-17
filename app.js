b84ce128b1749ccfd566748402ce723f44fb2eca
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-17 10:58:00 +0800

    S15 Sign up function

--- app.js
+++ app.js
@@ -59,19 +59,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Admin',
-          email: 'admin@admin.com',
-          cart: {
-            items: []
-          }
-        })
-        user.save()
-      }
-    })
+  .then(result => {    
     app.listen(3000);
   })
   .catch(err => console.log(err));
