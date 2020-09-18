9365166cf41afc71f363e43f7fc4d94d0f7db9ac
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-18 10:24:47 +0800

    S15 remove the parameter isAuthenticated in the res.render since it is included in app.js middleware

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,8 +5,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false,
-    isAuthenticated: req.session.isLoggedIn
+    editing: false
   });
 };
 
@@ -82,8 +81,7 @@ exports.getProducts = (req, res, next) => {
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
-        path: '/admin/products',
-        isAuthenticated: req.session.isLoggedIn
+        path: '/admin/products'
       });
     }).catch(err => {
       console.log(err)
