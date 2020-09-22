4fddcc77c1675f019370fa8e15d3ec06a3f25796
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-09-22 11:50:09 +0800

    also check the password passing in the body

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -13,17 +13,22 @@ router.post('/login', authController.postLogin);
 
 router.post(
   '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter valid email')
-    .custom((value , {req}) => {
-      if (value === 'test@test.com'){
-        //throw error to custom the error message
-        throw new Error('This email address is forbidden.');  
-        //or return false to indicate invalid
-      }
-      return true;
-    }),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter valid email')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          //throw error to custom the error message
+          throw new Error('This email address is forbidden.');
+          //or return false to indicate invalid
+        }
+        return true;
+      }),
+    body('password', 'Please enter a password with only numbers and text and at least 5 characters.')
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+  ],
   authController.postSignup
 );
 
