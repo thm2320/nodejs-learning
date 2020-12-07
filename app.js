fa4435218136557ade760c82e268a9e27b382bd8
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-12-07 13:55:56 +0800

    S19 error handling page

--- app.js
+++ app.js
@@ -45,18 +45,24 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  if (!req.session.user){
+  if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next()
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      throw new Error(err);
+
+    });
 });
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
@@ -66,11 +72,13 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
 
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {    
+  .then(result => {
     app.listen(3000);
   })
   .catch(err => console.log(err));
--- controllers/error.js
+++ controllers/error.js
@@ -1,8 +1,17 @@
 exports.get404 = (req, res, next) => {
   //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 
-  res.status(404).render('404',{
-      pageTitle: 'Page not Found',
-      path:'/404'
+  res.status(404).render('404', {
+    pageTitle: 'Page not Found',
+    path: '/404'
+  });
+};
+
+exports.get500 = (req, res, next) => {
+
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
   });
 };
